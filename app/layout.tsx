fc379cf346685ab55099d600cadf9d6735b5326c
happybear-21 <mr.rudradityathakur@gmail.com>
2025-09-18 15:47:13 +0530

    website setup

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import '@/app/global.css';
+import { RootProvider } from 'fumadocs-ui/provider';
+import { Poppins } from 'next/font/google';
+import type { ReactNode } from 'react';
+
+const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en" className={poppins.className} suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen">
+        <RootProvider>{children}</RootProvider>
+      </body>
+    </html>
+  );
+}
